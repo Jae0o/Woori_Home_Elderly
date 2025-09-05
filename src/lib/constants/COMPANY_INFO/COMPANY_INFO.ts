3ed7b0068034273bfa5f3d1f149cc4fd7a6d7041
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-09-05 21:43:42 +0900

    ✏️ Feat :  COMPANY_INFO 상수 생성

---
+++ src/lib/constants/COMPANY_INFO/COMPANY_INFO.ts
@@ -0,0 +1,6 @@
+const COMPANY_INFO = {
+  FAX: "063) 277 - 2538",
+  TEL: "063) 266 - 2538",
+} as const;
+
+export default COMPANY_INFO;
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,1 @@
+export { default as COMPANY_INFO } from "./COMPANY_INFO/COMPANY_INFO";
