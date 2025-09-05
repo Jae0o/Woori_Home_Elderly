f1d336043e0a17211d89ac9f830c7f044570845c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-09-05 22:56:28 +0900

    ✏️ Feat :  Introduce 이미지 추가

--- src/app/Introduce/IntroducePage.tsx
+++ src/app/Introduce/IntroducePage.tsx
@@ -1,8 +1,16 @@
+import { IntroduceImage } from "@lib/assets";
+
 import { Introduce } from "./components";
 
 const IntroducePage = () => {
   return (
-    <section className="w-full h-full p-[2rem] flex flex-col">
+    <section className="w-full h-full p-[2rem] py-[10rem] flex flex-col items-center">
+      <img
+        className="w-full max-w-[100rem]"
+        src={IntroduceImage}
+        alt="introduce"
+      />
+
       <Introduce />
     </section>
   );
--- src/lib/assets/index.ts
+++ src/lib/assets/index.ts
@@ -1,1 +1,2 @@
 export { default as BannerImage } from "./Banner_Logo.png";
+export { default as IntroduceImage } from "./Introduce_Image.png";
