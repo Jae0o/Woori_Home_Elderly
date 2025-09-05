9879db3bbc53fa18dd795f9b5c772b728a35b86f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-09-05 20:26:11 +0900

    🔖 Release :  v0.0.0 프로젝트 셋업 및 시작

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <main className="w-full h-full flex items-center justify-center">배포 테스트용 입니다. 듀규듀규</main>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import "./main.css";
+
+import { createRoot } from "react-dom/client";
+
+import App from "./App.tsx";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
