af70962affd835831f74527e07c593e4c08aa1a1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-09-05 23:58:51 +0900

    ✏️ Feat : NavbarMenus 컴포넌트의 제목을 '센터 소개'로 변경하고 프로그램 소개 항목 주석 처리

--- src/lib/layout/RootLayout/components/Navbar/components/NavbarMenus/NavbarMenus.tsx
+++ src/lib/layout/RootLayout/components/Navbar/components/NavbarMenus/NavbarMenus.tsx
@@ -7,13 +7,13 @@ const NavbarMenus = () => {
     <ul className="w-full h-[6rem] flex border-gray border-b-[0.1rem]">
       <NavbarMenusItems
         to={ROUTE_PATH.INTRODUCE}
-        title="회사소개"
+        title="센터 소개"
       />
 
-      <NavbarMenusItems
+      {/* <NavbarMenusItems
         to={ROUTE_PATH.PROGRAM}
         title="프로그램 소개"
-      />
+      /> */}
 
       <NavbarMenusItems
         to={ROUTE_PATH.MAP}
