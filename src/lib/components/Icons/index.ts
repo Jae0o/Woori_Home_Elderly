26ac7f7e5eeb29766ee45795ff9a930bf19d9ff1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-09-15 21:56:15 +0900

    ✏️ Feat :  ModeStandby Icon 생성

--- src/lib/components/Icons/index.ts
+++ src/lib/components/Icons/index.ts
@@ -1,2 +1,3 @@
 export { default as CallIcon } from "./CallIcon/CallIcon";
 export { default as FaxIcon } from "./FaxIcon/FaxIcon";
+export { default as ModeStandbyIcon } from "./ModeStandbyIcon/ModeStandbyIcon";
