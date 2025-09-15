133e571a3eb7636e6dcade59c07a6186382ae9d8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-09-15 23:16:17 +0900

    ✏️ Feat : CloseIcon 구현

--- src/lib/components/Icons/index.ts
+++ src/lib/components/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as CallIcon } from "./CallIcon/CallIcon";
 export { default as FaxIcon } from "./FaxIcon/FaxIcon";
 export { default as ModeStandbyIcon } from "./ModeStandbyIcon/ModeStandbyIcon";
+export { default as CloseIcon } from "./CloseIcon/CloseIcon";
