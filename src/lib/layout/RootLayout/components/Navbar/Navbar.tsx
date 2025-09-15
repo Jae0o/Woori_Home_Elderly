936418622f77186f7d49e00381781d714b89889e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-09-15 21:26:11 +0900

    🔨 Modify: 상단 메뉴바 연락처 disabled

--- src/lib/layout/RootLayout/components/Navbar/Navbar.tsx
+++ src/lib/layout/RootLayout/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import { BannerImage } from "@lib/assets";
-import { CallIcon, FaxIcon } from "@lib/components";
-import { COMPANY_INFO } from "@lib/constants";
+
+// import { CallIcon, FaxIcon } from "@lib/components";
+// import { COMPANY_INFO } from "@lib/constants";
 
 import { NavbarMenus } from "./components";
 
-import { twMerge } from "tailwind-merge";
+// import { twMerge } from "tailwind-merge";
 
 const Navbar = () => {
   return (
@@ -16,7 +17,7 @@ const Navbar = () => {
           alt=" banner"
         />
 
-        <span
+        {/* <span
           className={twMerge(
             "grow-1 h-full px-[4rem] flex flex-col justify-center items-end font-[500] text-[1.6rem] text-black",
             "max-mobile:hidden",
@@ -28,7 +29,7 @@ const Navbar = () => {
           <p className="flex items-center gap-[0.6rem]">
             <FaxIcon size="2rem" /> {COMPANY_INFO.FAX}
           </p>
-        </span>
+        </span> */}
       </div>
 
       <NavbarMenus />
