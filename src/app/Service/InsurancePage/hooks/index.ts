e83f95e8c21115ed1f532011c9d0d20a9b7a88af
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-10-21 19:04:28 +0900

    ✨ Feat: Add hooks for insurance data export

---
+++ src/app/Service/InsurancePage/hooks/index.ts
@@ -0,0 +1,8 @@
+export { useInsuranceData } from "./useInsuranceData";
+export type {
+  GradeTableRow,
+  InsuranceData,
+  InsuranceIntroduction,
+  InsuranceQualificationDetail,
+  PolicyCard,
+} from "./useInsuranceData";
