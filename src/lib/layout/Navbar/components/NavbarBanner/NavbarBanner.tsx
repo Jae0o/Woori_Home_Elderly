024dc22710efb78b5cd3f082c94847dce070bca2
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-12-06 19:55:42 +0900

    ♻️ Refactor : Logo Link 지정

--- src/lib/layout/Navbar/components/NavbarBanner/NavbarBanner.tsx
+++ src/lib/layout/Navbar/components/NavbarBanner/NavbarBanner.tsx
@@ -1,19 +1,24 @@
 import { WooriLogo } from "@lib/assets";
 
+import { Link } from "react-router";
+
 const NavbarBanner = () => {
   return (
-    <div className="h-[8rem] flex items-center select-none pointer-events-none">
+    <Link
+      to="/"
+      className="h-[8rem] flex items-center select-none cursor-pointer"
+    >
       <img
         className="w-[8rem] h-[8rem] object-contain"
         src={WooriLogo}
-        alt=" banner"
+        alt="우리재가노인복지센터 로고"
       />
 
       <span className="h-[8rem] pb-[1rem] flex flex-col justify-center font-bold">
         <p className="text-primary text-[2.2rem]">우리</p>
         <p className="text-[1.6rem] leading-[1.6rem]">재가노인복지센터</p>
       </span>
-    </div>
+    </Link>
   );
 };
 
